9772768d3054cf19a40d18226a7f036fa75f387d
Rulio974 <57720375+Rulio974@users.noreply.github.com>
2023-06-27 11:00:10 +0200

    load

--- lib/classic/home.dart
+++ lib/classic/home.dart
@@ -1,8 +1,11 @@
 import 'package:dmarc_flutter/classic/charts.dart';
 import 'package:dmarc_flutter/classic/table.dart';
+import 'package:dmarc_flutter/config/const_var.dart';
 import 'package:dmarc_flutter/error_snapshot.dart';
 import 'package:flutter/material.dart';
 import 'package:pluto_grid/pluto_grid.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:lottie/lottie.dart';
 
 import '../config/fetch_data.dart';
 
@@ -27,13 +30,23 @@ class _ClassicHomePageState extends State<ClassicHomePage> {
     return Scaffold(
         // backgroundColor: hqYellow,
         body: FutureBuilder(
-      future: Future.wait([fetchData(), fetchFileCount()]),
+      future: Future.wait([
+        Future.delayed(
+          const Duration(seconds: delayTime),
+          () => fetchData(),
+        ),
+        Future.delayed(
+          const Duration(seconds: delayTime),
+          () => fetchFileCount(),
+        ),
+      ]),
       builder: ((BuildContext context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-              child: CircularProgressIndicator(
-            color: Theme.of(context).iconTheme.color,
-          ));
+          return Center(child: Lottie.asset("load.json")
+              // child: SpinKitDoubleBounce(
+              //   color: Colors.blue,
+              // ),
+              );
         } else if (snapshot.hasData) {
           return Row(
             children: [
--- lib/classic/widgets/header_tab.dart
+++ lib/classic/widgets/header_tab.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import '../../config/const_var.dart';
 
-class HeaderTab extends StatelessWidget {
+class HeaderTab extends StatefulWidget {
   final Icon icone;
   final String title;
   final void Function() tabFunction;
 
   const HeaderTab({
-    super.key,
+    Key? key,
     required this.icone,
     required this.title,
     required this.tabFunction,
-  });
+  }) : super(key: key);
+
+  @override
+  _HeaderTabState createState() => _HeaderTabState();
+}
+
+class _HeaderTabState extends State<HeaderTab> {
+  bool isHovered = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,46 +27,53 @@ class HeaderTab extends StatelessWidget {
     double width = screenSize.width;
     double height = screenSize.height;
 
+    double containerHeight = isHovered ? height / 18 : height / 20;
+
     return Expanded(
-      child: InkWell(
-        onTap: () {
-          tabFunction();
-        },
-        child: Container(
-          height: height / 20,
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-            borderRadius: BorderRadius.circular(30),
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).shadowColor,
-                spreadRadius: 5,
-                blurRadius: 7,
-              ),
-            ],
-          ),
-          child: Center(
-            child: Wrap(
-              crossAxisAlignment: WrapCrossAlignment.center,
-              alignment: WrapAlignment.spaceAround,
-              children: [
-                FittedBox(
-                  fit: BoxFit.contain,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: icone,
-                  ),
-                ),
-                FittedBox(
-                  fit: BoxFit.contain,
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      color: Theme.of(context).hintColor,
-                    ),
-                  ),
+      child: MouseRegion(
+        onEnter: (_) => setState(() => isHovered = true),
+        onExit: (_) => setState(() => isHovered = false),
+        child: InkWell(
+          onTap: () {
+            widget.tabFunction();
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 100),
+            height: height / 20,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).shadowColor,
+                  blurRadius: isHovered ? 10 : 5,
+                  spreadRadius: isHovered ? 5 : 2,
                 ),
               ],
+            ),
+            child: Center(
+              child: Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                alignment: WrapAlignment.spaceAround,
+                children: [
+                  FittedBox(
+                    fit: BoxFit.contain,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: widget.icone,
+                    ),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.contain,
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(
+                        color: Theme.of(context).hintColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
